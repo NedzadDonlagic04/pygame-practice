87a96b2a6dd9cb2d24b67d0270ff95f387d8df41
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-09-15 18:24:00 +0200

    Finished the fourth practice and added comments

--- Practice_4/main.py
+++ Practice_4/main.py
@@ -1,3 +1,6 @@
+# Using the rectangle system I added the player (not movable atm)
+# and made the snail move using that system
+
 import pygame
 from sys import exit
 
@@ -17,8 +20,12 @@ text_img = text_font.render('My game', False, 'Black')
 text_pos = (350, 50) 
 
 snail_img = pygame.image.load('./../images/snail/snail1.png').convert_alpha()
-snail_y = 270
-snail_x = 730
+# Creating a rectangle with the same size as the image
+# and setting it's bottom left position so that it exactly touches the ground
+snail_rect = snail_img.get_rect( bottomleft = (800, 300))
+
+player_img = pygame.image.load('./../images/player/player_walk_1.png').convert_alpha()
+player_rect = player_img.get_rect( midbottom = (80, 300))
 
 while True:
     for event in pygame.event.get():
@@ -30,11 +37,19 @@ while True:
     screen.blit(ground_img, ground_pos)
     screen.blit(text_img, text_pos)
 
-    snail_x -= 3
-    screen.blit(snail_img, (snail_x, snail_y))
+    # Decreasing the snail rectangle's x value so it moves to the left
+    snail_rect.x -= 3
 
-    if snail_x < -60:
-        snail_x = 800
+    # In case the right value is under or equal to 0 that means the
+    # snail disappeared off the screen and so I put it back on the
+    # far left side
+    if snail_rect.right <= 0:
+        snail_rect.left = 800
+
+    # Placing the player and the snail to the coordinates of their
+    # squares
+    screen.blit(player_img, player_rect)
+    screen.blit(snail_img, snail_rect)
 
     pygame.display.update()
     clock.tick(60)
